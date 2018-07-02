353e8444ed8d2c96f835d8fc6a2c32390e9e3b8e
王晓勇 <plortinus@gmail.com>
2018-07-02 16:03:41 +0800

    init

--- src/index.js
+++ src/index.js
@@ -1,3 +1,17 @@
-import gcd from 'gcd-lib'
+import gcd from './gcd.js'
 
-console.log(gcd(14, 28))
+var app = new Vue({
+  el: '#app',
+  data: {
+    x: 1,
+    y: 2
+  },
+  computed: {
+    z () {
+      if (!this.x || !this.y) {
+        return ''
+      }
+      return gcd(this.x, this.y)
+    }
+  }
+})
